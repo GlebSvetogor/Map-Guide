ab20b8527f219ad496967d92540a4eec6fdcc573
Gleb Svetogor <asabsiu@mail.ru>
2023-12-10 23:13:09 +0300

    Added distance algorithm

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -63,10 +63,29 @@ namespace ConsoleApp2
                             {
                                 await IsCityAsync(el, botClient, chatId);
                             }
-                            foreach (string city in cities)
+
+                            int[,] distance = new int[cities.Count, cities.Count];
+                            for (int i = 0; i < cities.Count; i++)
                             {
-                                Console.WriteLine(city);
+                                for (int j = 0; j < cities.Count; j++)
+                                {
+                                    if (i == j)
+                                    {
+                                        distance[i, j] = 0;
+                                        continue;
+                                    }
+                                    DistanceBetweenCities(cities[i], cities[j], botClient,chatId);
+                                }
                             }
+
+                            //for (int i = 0; i < cities.Count; i++)
+                            //{
+                            //    for (int j = 0; j < cities.Count; j++)
+                            //    {
+
+                            //        Console.WriteLine(distance[i, j]);
+                            //    }
+                            //}
 
                         }
                         else
@@ -108,6 +127,57 @@ namespace ConsoleApp2
                     await botClient.SendTextMessageAsync(chatId, $"{city} не является городом.");
                 }
             }
+        }
+
+        private static async Task DistanceBetweenCities(string city1, string city2, ITelegramBotClient botClient, long chatId)
+        {
+            string username = "demo654";
+            string url1 = $"http://api.geonames.org/searchJSON?q={city1}&maxRows=1&username={username}&featureClass=P";
+            string url2 = $"http://api.geonames.org/searchJSON?q={city2}&maxRows=1&username={username}&featureClass=P";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response1 = await client.GetAsync(url1);
+                HttpResponseMessage response2 = await client.GetAsync(url2);
+                response1.EnsureSuccessStatusCode();
+                response2.EnsureSuccessStatusCode();
+                string responseBody1 = await response1.Content.ReadAsStringAsync();
+                string responseBody2 = await response2.Content.ReadAsStringAsync();
+
+                JObject json1 = JObject.Parse(responseBody1);
+                JObject json2 = JObject.Parse(responseBody2);
+                JArray geonames1 = (JArray)json1["geonames"];
+                JArray geonames2 = (JArray)json2["geonames"];
+                double lat1 = (double)geonames1[0]["lat"];
+                double lon1 = (double)geonames1[0]["lng"];
+                double lat2 = (double)geonames2[0]["lat"];
+                double lon2 = (double)geonames2[0]["lng"];
+
+                double distance = Distance(lat1, lon1, lat2, lon2);
+                
+                Console.WriteLine($"Расстояние между {city1} и {city2} составляет {distance} км.");
+            }
+
+        }
+
+        private static double Distance(double lat1, double lon1, double lat2, double lon2)
+        {
+            double R = 6371; // Radius of the earth in km
+            double dLat = Deg2Rad(lat2 - lat1);  // deg2rad below
+            double dLon = Deg2Rad(lon2 - lon1);
+            double a =
+              Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+              Math.Cos(Deg2Rad(lat1)) * Math.Cos(Deg2Rad(lat2)) *
+              Math.Sin(dLon / 2) * Math.Sin(dLon / 2)
+              ;
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double d = R * c; // Distance in km
+            return d;
+        }
+
+        private static double Deg2Rad(double deg)
+        {
+            return deg * (Math.PI / 180);
         }
 
         private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
